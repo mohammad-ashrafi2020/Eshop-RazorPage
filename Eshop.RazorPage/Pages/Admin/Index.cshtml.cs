5dd9e8825099079d1c355e7e92fc865d4a4aa3c5
mohammad ashrafi <mohammad.ashrafi2020@outlook.com>
2022-05-07 21:15:33 +0430

    create admin Layout

---
+++ Eshop.RazorPage/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Eshop.RazorPage.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
