d9d8c7a57ad3b718d8d1ca5645dcbeac2e93f741
محمد اشرافی <Mohammad.ashrafi2020@outlook.com>
2022-07-03 14:11:44 +0430

    Dynamic Menu

--- Eshop.RazorPage/Models/Banners/BannerDto.cs
+++ Eshop.RazorPage/Models/Banners/BannerDto.cs
@@ -11,5 +11,8 @@ public class BannerDto : BaseDto
 public enum BannerPosition
 {
     زیر_اسلایدر,
-    سمت_راست_اسلایر
+    سمت_چپ_اسلایدر,
+    بالای_اسلایدر,
+    سمت_راست_شگفت_انگیز,
+    وسط_صفحه
 }
